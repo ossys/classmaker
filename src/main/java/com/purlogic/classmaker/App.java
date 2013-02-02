ccafe978a1d367eb1218b9ac2af6e53a717f2cef
git <git@pur-logic.biz>
2013-02-02 17:12:55 -0500

    init

---
+++ src/main/java/com/purlogic/classmaker/App.java
@@ -0,0 +1,13 @@
+package com.purlogic.classmaker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
